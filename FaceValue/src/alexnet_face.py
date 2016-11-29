918a23b5c45575c1be441f9b66a83517d7e41e70
Tom Bartissol <tom.bartissol13@imperial.ac.uk>
2016-11-29 16:31:57 +0000

    alex net

--- FaceValue/src/alexnet_face.py
+++ FaceValue/src/alexnet_face.py
@@ -1,11 +1,39 @@
 import tensorflow.contrib.slim as slim
 import tensorflow as tf
 
+# each max pool layer has stride=2 and padding='VALID' by default
 
 def alexnet(inputs):
-  with slim.arg_scope([slim.ops.conv2d], weight_decay=0.0005,
-                                         activation_fn=None):
-  with arg_scope([slim.ops.batch_norm], activation_fn=tf.nn.relu):
-    net = slim.ops.conv2d(inputs, 96, [11, 11], stride=4, scope='conv1')
+  with slim.arg_scope([slim.conv2d], activation_fn=None, padding='VALID'):
+    with slim.arg_scope([slim.batch_norm], activation_fn=tf.nn.relu):
+      net = slim.conv2d(inputs, 96, [11, 11], stride=4, scope='conv1')
+      net = slim.batch_norm(net, scope='bn1')
+      net = slim.max_pool2d(net, [3, 3], scope='pool1')
+      
+      net = slim.conv2d(net, 256, [5, 5], stride=1, scope='conv2')
+      net = slim.batch_norm(net, scope='bn2')
+      net = slim.max_pool2d(net, [3, 3], scope='pool2')
+
+      net = slim.conv2d(net, 384, [3, 3], stride=1, scope='conv3')
+      net = slim.batch_norm(net, scope='bn3')
+
+      net = slim.conv2d(net, 384, [3, 3], stride=1, scope='conv4')
+      net = slim.batch_norm(net, scope='bn4')
+      
+      net = slim.conv2d(net, 256, [3, 3], stride=1, scope='conv5')
+      net = slim.batch_norm(net, scope='bn5')
+
+      net = slim.fully_connected(net, 4096, activation_fn=None, scope='fc6')
+      net = slim.batch_norm(net, scope='bn6')
+
+      net = slim.fully_connected(net, 4096, activation_fn=None, scope='fc7')
+      net = slim.batch_norm(net, scope='bn7')
+
+      net = slim.fully_connected(net, 2622, activation_fn=None, scope='fc8')
 
   return net
+
+
+if __name__ == '__main__':
+  inputs = tf.placeholder(tf.float32, (1, 227, 227, 3))
+  net = alexnet(inputs)
--- FaceValue/src/matflow.py
+++ FaceValue/src/matflow.py
@@ -10,24 +10,63 @@ def mat2tf(fname, name='mat2tf_translation'):
   layers = net_desc['layers'][0]
   trainOpts = net_desc['meta']['trainOpts'][0][0]
   # get shape of input image
-  in_shape = tuple(net_desc['meta']['inputSize'][0][0][0])
-  inputs = tf.placeholder(tf.int32, in_shape)
+  in_shape = extract_arr(net_desc['meta']['inputSize'])
+  in_shape = tuple(np.insert(in_shape,0,1))
+  print(in_shape)
+  inputs = tf.placeholder(tf.float32, in_shape)
   net = build_net(inputs, layers)
 
-def build_conv2d(layer_desc):
-  print('building: conv2d')
+def PADDING(n):
+  if n == 0:
+    return 'VALID'
+  else:
+    return 'SAME'
 
-def build_batch_norm(layer_desc):
-  print('building: batch_norm')
+def build_conv2d(net, layer_desc):
+  print('Layer: conv2d')
+  scope = extract_val(layer_desc['name'])
+  weights = extract_arr(layer_desc['weights'])
+  b_shape = weights[2].shape
+  print(b_shape)
+  stride = extract_val(layer_desc['stride'])
+  stride = [1, stride, stride, 1]
+  pad = extract_val(layer_desc['pad'])
+  print('  - scope  : {}'.format(scope))
+  print('  - weights: {}'.format(weights[0].shape))
+  print('  - biases : {}'.format(weights[1].shape))
+  print('  - stride : {}'.format(stride))
+  print('  - pading : {}'.format(pad))
+  with tf.name_scope(scope) as scope:
+    kernel = tf.Variable(weights[0], name='weights')
+    net = tf.nn.conv2d(net, kernel, stride, padding=PADDING(pad))
+    biases = tf.Variable(weights[1], trainable=True, name='biases')
+    net = tf.nn.bias_add(net, biases)
+  return net
 
-def build_relu(layer_desc):
-  print('building: relu')
+  
 
-def build_pool(layer_desc):
-  print('building: pool')
+def build_batch_norm(net, layer_desc):
+  print('Layer: batch_norm')
+  weights = extract_arr(layer_desc['weights'])
+  print('  - scope  : {}'.format(extract_val(layer_desc['name'])))
+  print('  - weights: {}'.format(weights.shape))
+  print('  - weight : {}'.format(weights[0].shape))
 
-def build_softmaxloss(layer_desc):
-  print('building: softmaxloss')
+def build_relu(net, layer_desc):
+  print('Layer: relu')
+  print('  - scope  : {}'.format(extract_val(layer_desc['name'])))
+  print('  - leak   : {}'.format(extract_val(layer_desc['leak'])))
+
+def build_pool(net, layer_desc):
+  print('Layer: pool')
+  print('  - scope  : {}'.format(extract_val(layer_desc['name'])))
+  print('  - method : {}'.format(extract_val(layer_desc['method'])))
+  print('  - pading : {}'.format(extract_val(layer_desc['pad'])))
+  print('  - stride : {}'.format(extract_val(layer_desc['stride'])))
+  print('  - kernel : {}'.format(extract_arr(layer_desc['pool'])))
+
+def build_softmaxloss(net, layer_desc):
+  print('Layer: softmaxloss')
 
 # dictionnary of building functions
 layer_builder = {
@@ -45,11 +84,20 @@ def build_net(inputs, layers):
   for i, l in enumerate(layers):
     l_type = extract_val(l['type'])
     # call approriate function to build this layer type
-    net = layer_builder[l_type](l)
+    net = layer_builder[l_type](net, l)
 
 def extract_val(arr):
   while isinstance(arr, (np.ndarray)):
     arr = arr[0]
+  return arr
+
+def extract_arr(arr):
+  while arr.shape == (1,1) or arr.shape == (1,):
+    arr = arr[0]
+  # get rid of redundant index ...
+  if len(arr.shape) == 2:
+    if arr.shape[0] == 1:
+      arr = arr.reshape((arr.shape[1],))
   return arr
 
 
